e107242f0bb1850a1257e57864f39ed1019740f7
bengisu <bengisugelin@gmail.com>
2023-07-05 22:26:45 -0700

    experts controller mapping corrected

--- src/main/java/com/project/basebeauty/webApi/controllers/ExpertsController.java
+++ src/main/java/com/project/basebeauty/webApi/controllers/ExpertsController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/experts")
+@Controller
+@RequestMapping()
 public class ExpertsController {
 
     private ExpertService expertService;
