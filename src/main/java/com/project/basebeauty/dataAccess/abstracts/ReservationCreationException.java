5e90b137748a6d2fc1a424de6519b0e628e2072d
javiermm14 <sakikookazawa@gmail.com>
2023-07-20 09:29:09 -0700

    Revert "reservation part"
    This reverts commit 6b0f2b92ed194ab447ac8f377436ad8359b0caab.

--- src/main/java/com/project/basebeauty/dataAccess/abstracts/ReservationCreationException.java
+++
@@ -1,7 +0,0 @@
-package com.project.basebeauty.dataAccess.abstracts;
-
-public class ReservationCreationException extends RuntimeException {
-    public ReservationCreationException(String message, Exception cause) {
-        super(message, cause);
-    }
-}
